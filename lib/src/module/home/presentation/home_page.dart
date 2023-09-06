f7f1b8a044d3b792b0cc979d470aa767c4c1d46b
devtvas <tarcisio.word@gmail.com>
2023-09-06 01:24:12 -0300

    feat: conclued challenge

--- lib/src/module/home/presentation/home_page.dart
+++ lib/src/module/home/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String? studentName, studentId, studyProgramId;
+  String? studentName, studentID, studyProgramID;
   double? studentGPA;
 
   getStudentName(name) {
@@ -18,11 +19,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   getStudentID(id) {
-    studentId = id;
+    studentID = id;
   }
 
   getStudyProgramID(programID) {
-    studyProgramId = programID;
+    studyProgramID = programID;
   }
 
   getStudentGPA(gpa) {
@@ -30,19 +31,64 @@ class _HomePageState extends State<HomePage> {
   }
 
   createData() {
-    log('create');
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('Students').doc('$studentName');
+    //
+    Map<String, dynamic> students = {
+      'studentName': studentName,
+      'studentID': studentID,
+      'studyProgramID': studyProgramID,
+      'studentGPA': studentGPA,
+    };
+    //
+    documentReference
+        .set(students)
+        .whenComplete(() => {log("$studentName created")});
   }
 
   readData() {
-    log('read');
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('Students').doc('$studentName');
+    //
+    documentReference.get().then(
+      (datasnapshot) {
+        if (datasnapshot.id.contains('$studentName')) {
+          Map<String, dynamic> data =
+              datasnapshot.data() as Map<String, dynamic>;
+          // log("Full Name: ${data.values}");
+          log("${data['studentName']}");
+          log("${data['studentID']}");
+          log("${data['studyProgramID']}");
+          log("${data['studentGPA']}");
+        }
+      },
+    );
   }
 
   updateData() {
-    log('update');
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection('Students').doc('$studentName');
+    //
+    Map<String, dynamic> students = {
+      'studentName': studentName,
+      'studentID': studentID,
+      'studyProgramID': studyProgramID,
+      'studentGPA': studentGPA,
+    };
+    //
+    documentReference
+        .update(students)
+        .whenComplete(() => {log("$studentName updated")});
   }
 
   deleteData() {
-    log('delet');
+    CollectionReference documentReference =
+        FirebaseFirestore.instance.collection('Students');
+
+    documentReference
+        .doc('$studentName')
+        .delete()
+        .whenComplete(() => {log("$studentName deleted")});
   }
 
   @override
@@ -51,127 +97,182 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Firebase'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  ),
-                ),
-                onChanged: (String name) {
-                  getStudentName(name);
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Student ID',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  ),
-                ),
-                onChanged: (String id) {
-                  getStudentID(id);
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Study Program ID',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  ),
-                ),
-                onChanged: (String programID) {
-                  getStudyProgramID(programID);
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'GPA',
-                  fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  ),
-                ),
-                onChanged: (String gpa) {
-                  getStudentGPA(gpa);
-                },
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      createData();
-                    },
-                    child: Text('Create'),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.green),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
                     ),
                   ),
+                  onChanged: (String name) {
+                    getStudentName(name);
+                  },
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Read'),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Student ID',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
                     ),
                   ),
+                  onChanged: (String id) {
+                    getStudentID(id);
+                  },
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Update'),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.amber),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Study Program ID',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
                     ),
                   ),
+                  onChanged: (String programID) {
+                    getStudyProgramID(programID);
+                  },
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Delete'),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.red),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'GPA',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
                     ),
                   ),
+                  onChanged: (String gpa) {
+                    getStudentGPA(gpa);
+                  },
                 ),
-              ],
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        createData();
+                      },
+                      child: Text('Create'),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.green),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        readData();
+                      },
+                      child: Text('Read'),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        updateData();
+                      },
+                      child: Text('Update'),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.amber),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        deleteData();
+                      },
+                      child: Text('Delete'),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.red),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  textDirection: TextDirection.ltr,
+                  children: const [
+                    Expanded(child: Text('Name')),
+                    Expanded(child: Text('ID')),
+                    Expanded(child: Text('ProgramID')),
+                    Expanded(
+                      child: Text('GPA'),
+                    ),
+                  ],
+                ),
+              ),
+              StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection('Students').snapshots(),
+                builder: (context, snapshot) {
+                  return Container(
+                    child: snapshot.hasData
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!.docs.length,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot documentSnapshot =
+                                  snapshot.data!.docs[index];
+                              return Row(
+                                children: [
+                                  Expanded(
+                                      child:
+                                          Text(documentSnapshot['studentName'])),
+                                  Expanded(
+                                      child: Text(documentSnapshot['studentID'])),
+                                  Expanded(
+                                      child: Text(
+                                          documentSnapshot['studyProgramID'])),
+                                  Expanded(
+                                      child: Text(documentSnapshot['studentGPA']
+                                          .toString())),
+                                ],
+                              );
+                            },
+                          )
+                        : const CircularProgressIndicator(),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
